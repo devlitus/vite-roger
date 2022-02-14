28d5294bbc137a5daaf8ace76a7fd1be2bd7ca50
devlitus <developercarles@gmail.com>
2022-02-14 12:51:39 +0100

    new add component footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,30 @@
+import "./footer.css";
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <img
+          className="image-social"
+          src="/assets/resources/emailClar.png"
+          alt=""
+        />
+        <img
+          className="image-social"
+          src="/assets/resources/linkedinClar.png"
+          alt=""
+        />
+        <img
+          className="image-social"
+          src="/assets/resources/youtubeClar.png"
+          alt=""
+        />
+        <img
+          className="image-social"
+          src="/assets/resources/instagramClar.png"
+          alt=""
+        />
+        <p className="text-service">Serveis</p>
+      </div>
+    </footer>
+  );
+};
