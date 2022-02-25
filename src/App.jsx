fab53092de3922482c75a417a1337be15c2bd8b9
Carles <developercarles@gmail.com>
2022-02-25 12:05:02 +0100

    App
    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useTranslation } from "react-i18next";
 import { Route, Routes } from "react-router-dom";
 import "./app.css";
 import { Navbar } from "./components/navbar/Navbar";
 import { About } from "./pages/about/About";
-import { Bissnes } from "./pages/bissnes/bissnes";
+import { Bissnes } from "./pages/bissnes/Bissnes";
 import { Contact } from "./pages/contact/Contact";
 import { Family } from "./pages/family/Family";
 import { Home } from "./pages/home/Home";
+import { Training } from "./pages/training/Training";
 
 function App() {
-  const { t } = useTranslation();
-
   return (
     <div className="">
       <Navbar />
@@ -20,6 +18,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/family" element={<Family />} />
         <Route path="/bissnes" element={<Bissnes />} />
+        <Route path="/training" element={<Training />} />
       </Routes>
     </div>
   );
