d8fa719da2211bd5abffd7d9db9afe390bba87ea
Carles <developercarles@gmail.com>
2022-02-25 10:58:53 +0100

    Component Bissnes
    Modify styles and structure html

--- src/pages/bissnes/bissnes.jsx
+++ src/pages/bissnes/bissnes.jsx
@@ -6,7 +6,7 @@ export const Bissnes = () => {
   const { t } = useTranslation();
   return (
     <main className="family-wrapper">
-      <header className="family-header">
+      <header className="bissnes-header">
         <h1>{t("home.titleSection")}</h1>
         <img
           className="image-rotate"
@@ -14,46 +14,49 @@ export const Bissnes = () => {
           alt=""
         />
       </header>
-      <div className="family-content">
+      <div className="bissnes-content">
         <h1 className="">{t("bissnes.titleh1")}</h1>
         <p className="text">{t("bissnes.text1")}</p>
-        <h3 className="content-title">1- {t("family.talks.1.title")}</h3>
-        <div className="text-center">
-          <p className="text-center-bold">{t("family.talks.1.destination")}</p>
-          <p className="text-center-blue">{t("family.talks.1.object")}</p>
-          <p className="text-center-normal">{t("family.talks.1.objective")}</p>
+        <h3 className="bissnes-content-title">
+          1- {t("bissnes.talks.1.title")}
+        </h3>
+        <div className="text-center content-variant">
+          <p className="text-center-bold">{t("bissnes.talks.1.destination")}</p>
+          <p className="text-center-blue">{t("bissnes.talks.1.object")}</p>
+          <p className="text-center-normal">{t("bissnes.talks.1.objective")}</p>
+          <img
+            className="arrow-down"
+            src="/assets/resources/arrowDown.png"
+            alt=""
+          />
+          <div className="bissnes-polaroid-blue">
+            <CardPolaroid src="/assets/resources/polaroidBlueRect.png" />
+          </div>
         </div>
       </div>
-      <div className="point-polaroid">
+
+      <div className="bissnes-content ">
+        <h3 className="bissnes-content-title">
+          2- {t("bissnes.talks.2.title")}
+        </h3>
+        <div className="text-center ">
+          <p className="text-center-bold">{t("bissnes.talks.2.destination")}</p>
+          <p className="text-center-blue">{t("bissnes.talks.1.object")}</p>
+          <p className="text-center-normal">{t("bissnes.talks.2.objective")}</p>
+        </div>
+      </div>
+      <div className="bissnes-section-polaroid">
+        <div className="bissnes-polaroid">
+          <CardPolaroid src="/assets/resources/polaroidClaraRectaImage.png" />
+        </div>
+        <CardPolaroid src="/assets/resources/polaroidWhiteRect.png" />
+      </div>
+      <div>
         <img
           className="image-point"
           src="/assets/resources/pointsClaros.png"
           alt=""
         />
-        <div className="takes-polaroid-image">
-          <CardPolaroid src="/assets/resources/polaroidWhiteRect.png" />
-        </div>
-      </div>
-      <div className="family-content ">
-        <h3 className="content-title">2- {t("family.talks.2.title")}</h3>
-        <div className="text-center content-variant">
-          <p className="text-center-bold">{t("family.talks.2.destination")}</p>
-          <p className="text-center-blue">{t("family.talks.1.object")}</p>
-          <p className="text-center-normal">{t("family.talks.2.objective")}</p>
-          <img
-            className="arrow-up-rect"
-            src="/assets/resources/arrowUpRect.png"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className="section-polaroid">
-        <div className="takes-polaroid">
-          <CardPolaroid src="/assets/resources/polaroidClaraRectaImage.png" />
-        </div>
-        <div className="takes-polaroid-blue">
-          <CardPolaroid src="/assets/resources/polaroidBlueRect.png" />
-        </div>
       </div>
       <Footer />
     </main>
