3607c54d3c57002b641516032fb83ae8458428f3
Carles <developercarles@gmail.com>
2022-02-25 12:05:35 +0100

    Component Training
    New Component

---
+++ src/pages/training/Training.jsx
@@ -0,0 +1,38 @@
+import { useTranslation } from "react-i18next";
+import { Footer } from "../../components/footer/Footer";
+import "./training.css";
+
+export const Training = () => {
+  const { t } = useTranslation();
+  return (
+    <main className="family-wrapper">
+      <header className="training-header">
+        <h1>{t("home.titleSection")}</h1>
+        <img
+          className="image-rotate"
+          src="/assets/resources/arrow-rotate.png"
+          alt=""
+        />
+      </header>
+      <div className="training-content">
+        <h1 className="">{t("trining.titleh1")}</h1>
+        <p className="text">{t("trining.text1")}</p>
+        <h3 className="training-content-title">
+          1- {t("trining.talks.1.title")}
+        </h3>
+        <div className="text-center content-variant">
+          <p className="text-center-bold">{t("trining.talks.1.destination")}</p>
+          <p className="text-center-blue">{t("trining.talks.1.object")}</p>
+          <p className="text-center-normal">{t("trining.talks.1.objective")}</p>
+          <img
+            className="image-point"
+            src="/assets/resources/pointBrow.png"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <Footer background="#e18a30" />
+    </main>
+  );
+};
