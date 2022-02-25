839d5b04d6b00cc6aeb38b56fd54679426cafa38
Carles <developercarles@gmail.com>
2022-02-25 09:45:14 +0100

    Component App
    Add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useTranslation } from "react-i18next";
 import { Route, Routes } from "react-router-dom";
-import { Navbar } from "./components/navbar/Navbar";
-import { Home } from "./pages/home/Home";
-import { About } from "./pages/about/About";
-import { Contact } from "./pages/contact/Contact";
 import "./app.css";
+import { Navbar } from "./components/navbar/Navbar";
+import { About } from "./pages/about/About";
+import { Bissnes } from "./pages/bissnes/bissnes";
+import { Contact } from "./pages/contact/Contact";
+import { Family } from "./pages/family/Family";
+import { Home } from "./pages/home/Home";
 
 function App() {
   const { t } = useTranslation();
@@ -16,6 +18,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/family" element={<Family />} />
+        <Route path="/bissnes" element={<Bissnes />} />
       </Routes>
     </div>
   );
