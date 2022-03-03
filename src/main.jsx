643c96840ac2af0e5ef6a8db9ea535426190400f
Carles <developercarles@gmail.com>
2022-03-03 10:03:24 +0100

    Main
    refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { I18nextProvider } from "react-i18next";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import i18n from "./i18n";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { I18nextProvider } from 'react-i18next'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import i18n from './i18n'
 
 ReactDOM.render(
   <I18nextProvider i18n={i18n}>
@@ -12,5 +12,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </I18nextProvider>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
