7d3ae28595d810e45f0f38d41ed4d6b1543160c9
Carles <developercarles@gmail.com>
2022-03-03 09:56:39 +0100

    Component Footer
    add new component IconSocial

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,39 +1,39 @@
-import { IconSocial } from "../iconSocial/IconSocial";
-import "./footer.css";
+import { IconSocial } from '../iconSocial/IconSocial';
+import './footer.css';
 
 export const Footer = ({ background }) => {
   return (
-    <footer className="footer" style={{ backgroundColor: background }}>
-      <div className="footer-content">
-        <a href="mailto:rogerllopart@rogerllopart.net" target="_blank">
+    <footer className='footer' style={{ backgroundColor: background }}>
+      <div className='footer-content'>
+        <a href='mailto:rogerllopart@rogerllopart.net' target='_blank' rel='noreferrer'>
           <IconSocial
-            className="image-social"
-            src="/assets/resources/emailClar.png"
-            alt="email"
+            className='image-social'
+            src='/assets/resources/emailClar.png'
+            alt='email'
           />
         </a>
-        <a href="">
+        <a href=''>
           <IconSocial
-            className="image-social"
-            src="/assets/resources/linkedinClar.png"
-            alt="linkedin"
+            className='image-social'
+            src='/assets/resources/linkedinClar.png'
+            alt='linkedin'
           />
         </a>
-        <a href="">
+        <a href=''>
           <IconSocial
-            className="image-social"
-            src="/assets/resources/youtubeClar.png"
-            alt="youtube"
+            className='image-social'
+            src='/assets/resources/youtubeClar.png'
+            alt='youtube'
           />
         </a>
-        <a href="">
+        <a href=''>
           <IconSocial
-            className="image-social"
-            src="/assets/resources/instagramClar.png"
-            alt="instagram"
+            className='image-social'
+            src='/assets/resources/instagramClar.png'
+            alt='instagram'
           />
         </a>
-        <p className="text-service">Serveis</p>
+        <p className='text-service'>Serveis</p>
       </div>
     </footer>
   );
