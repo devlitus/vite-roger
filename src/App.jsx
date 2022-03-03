53c8a9703703dff4b0e54ca877e719d990681fcf
Carles <developercarles@gmail.com>
2022-03-03 09:54:28 +0100

    Router
    add new routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { Route, Routes } from "react-router-dom";
-import "./app.css";
-import { Navbar } from "./components/navbar/Navbar";
-import { About } from "./pages/about/About";
-import { Bissnes } from "./pages/bissnes/Bissnes";
-import { Contact } from "./pages/contact/Contact";
-import { Family } from "./pages/family/Family";
-import { Home } from "./pages/home/Home";
-import { Training } from "./pages/training/Training";
+import { Route, Routes } from 'react-router-dom'
+import './app.css'
+import { Navbar } from './components/navbar/Navbar'
+import { About } from './pages/about/About'
+import { Bissnes } from './pages/bissnes/Bissnes'
+import { Contact } from './pages/contact/Contact'
+import { Family } from './pages/family/Family'
+import { Home } from './pages/home/Home'
+import { Training } from './pages/training/Training'
 
-function App() {
+function App () {
   return (
-    <div className="">
+    <div className=''>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/family" element={<Family />} />
-        <Route path="/bissnes" element={<Bissnes />} />
-        <Route path="/training" element={<Training />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/family' element={<Family />} />
+        <Route path='/bissnes' element={<Bissnes />} />
+        <Route path='/training' element={<Training />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
