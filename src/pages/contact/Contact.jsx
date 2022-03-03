2349e5aadcd1f56e9ada58d30324f871fe3f1f87
Carles <developercarles@gmail.com>
2022-03-03 10:03:53 +0100

    Components
    refactor

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,64 +1,70 @@
-import { useTranslation } from "react-i18next";
-import { IconSocial } from "../../components/iconSocial/IconSocial";
-import "./contact.css";
+import { useTranslation } from 'react-i18next';
+import { IconSocial } from '../../components/iconSocial/IconSocial';
+import './contact.css';
 export const Contact = () => {
   const { t } = useTranslation();
   return (
-    <div className="contact-wrapper">
+    <div className='contact-wrapper'>
       <img
-        className="logo"
-        src="/assets/logo/logo.png"
-        alt="logo Roger Llopart"
+        className='logo'
+        src='/assets/logo/logo.png'
+        alt='logo Roger Llopart'
       />
-      <div className="contact-content">
-        <div className="content-icons-social">
+      <div className='contact-content'>
+        <div className='content-icons-social'>
           <a
-            href="https://www.linkedin.com/in/roger-llopart-09bb6926/"
-            target="_blank"
+            href='https://www.linkedin.com/in/roger-llopart-09bb6926/'
+            target='_blank'
+            rel='noreferrer'
           >
             <IconSocial
-              className="icon-linkedin"
-              src="/assets/resources/linkedin.png"
+              className='icon-linkedin'
+              src='/assets/resources/linkedin.png'
             />
           </a>
           <a
-            href="https://www.youtube.com/channel/UCudooYnD51UpQA8ayh64JMA"
-            target="_blank"
+            href='https://www.youtube.com/channel/UCudooYnD51UpQA8ayh64JMA'
+            target='_blank'
+            rel='noreferrer'
           >
             <IconSocial
-              className="icon-linkedin"
-              src="/assets/resources/youtube.png"
+              className='icon-linkedin'
+              src='/assets/resources/youtube.png'
             />
           </a>
-          <a href="https://www.instagram.com/rogerllopart/" target="_blank">
+          <a
+            href='https://www.instagram.com/rogerllopart/'
+            target='_blank'
+            rel='noreferrer'
+          >
             <IconSocial
-              className="icon-linkedin"
-              src="/assets/resources/instagram.png"
+              className='icon-linkedin'
+              src='/assets/resources/instagram.png'
             />
           </a>
         </div>
         <div>
-          <div className="content-phone-email">
+          <div className='content-phone-email'>
             <img
-              className="image-phone"
-              src="/assets/resources/phone.png"
-              alt=""
+              className='image-phone'
+              src='/assets/resources/phone.png'
+              alt=''
             />
-            <a href="mailto:rogerllopart@rogerllopart.net">
+            <a href='mailto:rogerllopart@rogerllopart.net'>
               <img
-                className="image-email"
-                src="/assets/resources/emailBlue.png"
-                alt=""
+                className='image-email'
+                src='/assets/resources/emailBlue.png'
+                alt=''
               />
             </a>
           </div>
         </div>
-        <div className="contact-cite">
-          <p>{t("contact.cite")}</p>
+        <div className='contact-cite'>
+          <p>{t('contact.cite')}</p>
         </div>
       </div>
-      <div className="box-point">
-        <img className="iamge-point" src="/assets/resources/point.png" alt="" />
+      <div className='box-point'>
+        <img className='iamge-point' src='/assets/resources/point.png' alt='' />
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,119 +1,119 @@
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
-import { CardPolaroid } from "../../components/card/CardPolaroid";
-import { Footer } from "../../components/footer/Footer";
-import { ImagePatern } from "../../components/paterns/patern";
-import { SectionResource } from "../../components/sectionResource/SectionResource";
-import "./home.css";
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { CardPolaroid } from '../../components/card/CardPolaroid';
+import { Footer } from '../../components/footer/Footer';
+import { ImagePatern } from '../../components/paterns/patern';
+import { SectionResource } from '../../components/sectionResource/SectionResource';
+import './home.css';
 
 export const Home = () => {
   const { t } = useTranslation();
   return (
-    <main className="home-wrapper">
-      <header className="home-header">
+    <main className='home-wrapper'>
+      <header className='home-header'>
         <img
-          className="home-image"
-          src="/assets/images/bg/bg-header.png"
-          alt="Roger Llopart"
+          className='home-image'
+          src='/assets/images/bg/bg-header.png'
+          alt='Roger Llopart'
         />
-        <p className="home-cite">
-          {t("home.cite")}.
+        <p className='home-cite'>
+          {t('home.cite')}.
           <br /> El Petit Princep
         </p>
       </header>
-      <div className="home-section">
+      <div className='home-section'>
         <img
-          className="home-arrow"
-          src="/assets/resources/arrow-rotate.png"
-          alt="arrow"
+          className='home-arrow'
+          src='/assets/resources/arrow-rotate.png'
+          alt='arrow'
         />
-        <p className="home-section-title">{t("home.titleSection")}</p>
+        <p className='home-section-title'>{t('home.titleSection')}</p>
       </div>
-      <div className="home-content">
-        <p className="home-content-text">{t("home.textSection")}</p>
+      <div className='home-content'>
+        <p className='home-content-text'>{t('home.textSection')}</p>
       </div>
-      <div className="home-polaroid">
-        <div className="polaroidClar">
-          <Link to="/family">
+      <div className='home-polaroid'>
+        <div className='polaroidClar'>
+          <Link to='/family'>
             <CardPolaroid
-              src="/assets/resources/polaroidClaraRectaImage.png"
-              title="XARRADES PER A FAMILIAS"
+              src='/assets/resources/polaroidClaraRectaImage.png'
+              title='XARRADES PER A FAMILIAS'
             />
           </Link>
         </div>
-        <div className="polaroidBlue">
-          <Link to="/bissnes">
+        <div className='polaroidBlue'>
+          <Link to='/bissnes'>
             <CardPolaroid
-              src="/assets/resources/polaroidBlueRect.png"
-              title="FORMACIONS PROFESIONALS EDUCACIÓ"
+              src='/assets/resources/polaroidBlueRect.png'
+              title='FORMACIONS PROFESIONALS EDUCACIÓ'
             />
           </Link>
         </div>
-        <div className="polaroidWhite">
-          <Link to="/training">
+        <div className='polaroidWhite'>
+          <Link to='/training'>
             <CardPolaroid
-              src="/assets/resources/polaroidWhiteRect.png"
-              title="LA FORMACIÓ BONIFICADA PER EMPRESAS"
+              src='/assets/resources/polaroidWhiteRect.png'
+              title='LA FORMACIÓ BONIFICADA PER EMPRESAS'
             />
           </Link>
         </div>
       </div>
-      <p className="home-title">vols saber més</p>
+      <p className='home-title'>vols saber més</p>
       <SectionResource />
-      <p className="home-title alter">Alguns del's meus clients</p>
-      <div className="home-paterns">
+      <p className='home-title alter'>Alguns del's meus clients</p>
+      <div className='home-paterns'>
         <ImagePatern
-          className="image-patern"
-          src="/assets/patners/lliureSport.png"
-          alt="lliure Sport"
+          className='image-patern'
+          src='/assets/patners/lliureSport.png'
+          alt='lliure Sport'
         />
         <ImagePatern
-          className="image-patern"
-          src="/assets/patners/cornella.jpg"
-          alt="Ajuntament de Cornella"
+          className='image-patern'
+          src='/assets/patners/cornella.jpg'
+          alt='Ajuntament de Cornella'
         />
         <ImagePatern
-          className="image-patern"
-          src="/assets/patners/carol.jpg"
-          alt="fundació escolta Josep Carol"
+          className='image-patern'
+          src='/assets/patners/carol.jpg'
+          alt='fundació escolta Josep Carol'
         />
         <ImagePatern
-          className="image-patern"
-          src="/assets/patners/jaume.png"
-          alt="centre educatiu Jaume Viladoms"
+          className='image-patern'
+          src='/assets/patners/jaume.png'
+          alt='centre educatiu Jaume Viladoms'
         />
         <ImagePatern
-          className="image-patern"
-          src="/assets/patners/andorra.png"
-          alt="Govern de Andorra"
+          className='image-patern'
+          src='/assets/patners/andorra.png'
+          alt='Govern de Andorra'
         />
         <ImagePatern
-          className="image-patern"
-          src="/assets/patners/doctor.png"
-          alt="col·legi oficial de docotrs"
+          className='image-patern'
+          src='/assets/patners/doctor.png'
+          alt='col·legi oficial de docotrs'
         />
         <ImagePatern
-          className="image-patern"
-          src="/assets/patners/fundesplai.jpg"
-          alt="Fundesplai"
+          className='image-patern'
+          src='/assets/patners/fundesplai.jpg'
+          alt='Fundesplai'
         />
         <ImagePatern
-          className="image-patern"
-          src="/assets/patners/radio.png"
-          alt="radio 4"
+          className='image-patern'
+          src='/assets/patners/radio.png'
+          alt='radio 4'
         />
         <ImagePatern
-          className="image-patern"
-          src="/assets/patners/fapac.png"
-          alt="FAPAC"
+          className='image-patern'
+          src='/assets/patners/fapac.png'
+          alt='FAPAC'
         />
       </div>
-      <section className="home-footer">
+      <section className='home-footer'>
         <Footer />
         <img
-          className="home-image-footer"
-          src="/assets/resources/arrow-rotate.png"
-          alt=""
+          className='home-image-footer'
+          src='/assets/resources/arrow-rotate.png'
+          alt=''
         />
       </section>
     </main>
