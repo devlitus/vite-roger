bf61956073349fb00abaaf71814973fbc97afee4
Carles <developercarles@gmail.com>
2022-03-03 09:59:40 +0100

    Component Navigator
    implement translation to links

--- src/components/navigator/Navigator.jsx
+++ src/components/navigator/Navigator.jsx
@@ -1,19 +1,19 @@
-import { faGlobe } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "animate.css";
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
-import i18n from "../../i18n";
-import "./navigator.css";
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import 'animate.css';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import i18n from '../../i18n';
+import './navigator.css';
 
 export const Navigation = () => {
   const [isHidden, setHidden] = useState(false);
-  const { isvisibility, setvisibility } = useState(false);
+  // const { isvisibility, setvisibility } = useState(false);
   const { language, changeLanguage } = i18n;
   const { t } = useTranslation();
   const handleChangeLanguage = () => {
-    changeLanguage(language === "ca" ? "es" : "ca");
+    changeLanguage(language === 'ca' ? 'es' : 'ca');
     setHidden(false);
   };
   const handleHidden = () => {
@@ -21,45 +21,45 @@ export const Navigation = () => {
     setHidden(true);
   };
   return (
-    <nav className="navigator">
-      <ul className="navigator-items">
-        <li className="navigator-item">
-          <Link className="navigator-link" to="/">
-            {t("home.pathUrl")}
+    <nav className='navigator'>
+      <ul className='navigator-items'>
+        <li className='navigator-item'>
+          <Link className='navigator-link' to='/'>
+            {t('home.pathUrl')}
           </Link>
         </li>
-        <li className="navigator-item">
-          <Link className="navigator-link" to="/about">
-            {t("about.pathUrl")}
+        <li className='navigator-item'>
+          <Link className='navigator-link' to='/about'>
+            {t('about.pathUrl')}
           </Link>
         </li>
-        <li className="navigator-item">
-          <Link className="navigator-link" to="/">
+        <li className='navigator-item'>
+          <Link className='navigator-link' to='/'>
             Campus
           </Link>
         </li>
-        <li className="navigator-item">
-          <Link className="navigator-link" to="/">
+        <li className='navigator-item'>
+          <Link className='navigator-link' to='/'>
             Blog
           </Link>
         </li>
-        <li className="navigator-item">
-          <Link className="navigator-link" to="/contact">
-            {t("contact.pathUrl")}
+        <li className='navigator-item'>
+          <Link className='navigator-link' to='/contact'>
+            {t('contact.pathUrl')}
           </Link>
         </li>
         <li>
           <FontAwesomeIcon
             icon={faGlobe}
-            className="navigator-globe"
+            className='navigator-globe'
             onClick={handleHidden}
           />
           <div
             className={`navigator-select  animate__animated ${
-              isHidden ? `animate__fadeInDown` : `navigator-select-hidden`
+              isHidden ? 'animate__fadeInDown' : 'navigator-select-hidden'
             }`}
           >
-            <button className="navigator-btn" onClick={handleChangeLanguage}>
+            <button className='navigator-btn' onClick={handleChangeLanguage}>
               {language}
             </button>
           </div>
